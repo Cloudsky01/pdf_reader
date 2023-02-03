fa8b4f1e95faf932ed8e09473076717ce7695b03
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2023-02-03 13:27:59 -0500

    feat/ reformat code

--- src/main.py
+++ src/main.py
@@ -3,25 +3,45 @@
 from PyPDF2 import PdfReader
 import pandas as pd
 import csv
+import os
  
-# creating a pdf reader object
-reader = PdfReader('./pdf/Test.pdf')
- 
-# printing number of pages in pdf file
-print(len(reader.pages))
- 
-# getting a specific page from the pdf file
-page = reader.pages[0]
- 
-# extracting text from page
-text = page.extract_text()
+def write_to_txt(text, path):
+    with open(path, 'w') as f:
+        f.write(text)
 
-with open('test.txt', 'w') as f:
-    f.write(text)
+def openPDF(path):
+    # creating a pdf reader object
+    reader = PdfReader(path)
+    
+    # printing number of pages in pdf file
+    print(len(reader.pages))
+    
+    # getting a specific page from the pdf file
+    page = reader.pages[0]
+    
+    # extracting text from page
+    text = page.extract_text()
+    formatted_txt_path = path.replace('pdf', 'txt')
+    write_to_txt(text, formatted_txt_path)
 
-with open('test.txt', 'r') as f:
-    stripped = (line.strip() for line in f)
-    lines = (line.split(",") for line in stripped if line)
-    with open('./csv/out.csv', 'w') as out_file:
-        writer = csv.writer(out_file)
-        writer.writerows(lines)
+# Function that iterate through the pdf directory
+def iterate_through_dir():
+    directory = 'pdf'
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        print("Opening file: {}".format(f))
+        openPDF(f)
+
+if __name__ == '__main__':
+    iterate_through_dir()
+
+
+# with open('test.txt', 'w') as f:
+#     f.write(text)
+
+# with open('test.txt', 'r') as f:
+#     stripped = (line.strip() for line in f)
+#     lines = (line.split(",") for line in stripped if line)
+#     with open('./csv/out.csv', 'w') as out_file:
+#         writer = csv.writer(out_file)
+#         writer.writerows(lines)
